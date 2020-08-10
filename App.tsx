ff4dfda8f3d6f5d07cf8be47270c98b1d0b31796
juliabresolin <juliabresolin2@gmail.com>
2020-08-10 09:48:44 -0300

    build: folders structure init and icons add

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/pages/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Home />
+      <StatusBar style="light" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './styles';
+import { View, Text } from 'react-native';
+
+function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Home works!</Text>
+    </View>
+  )
+};
+
+export default Home;
