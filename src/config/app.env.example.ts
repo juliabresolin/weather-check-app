6920350240aad87975514679878e403c62e74f91
jbresolinn <juliabresolin2@gmail.com>
2020-08-10 15:15:37 -0300

    perf: environments variables temp config

---
+++ src/config/app.env.example.ts
@@ -0,0 +1,9 @@
+interface keysProps {
+  openweather_api: string;
+}
+
+const appEnv: keysProps = {
+  openweather_api: ""
+}
+
+export default appEnv;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import keys from '../../config/app.env';
 
 import styles from './styles';
 import { View, Text } from 'react-native';
 
 function Home() {
+
+  function getLocation() {
+    const test = keys.openweather_api;
+    console.log(test)
+  }
+
+  getLocation();
+
   return (
     <View style={styles.container}>
       <Text>Home works!</Text>
