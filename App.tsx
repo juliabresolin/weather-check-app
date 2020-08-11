b8911686d26e05b9214deb12e60332705c7c5d5d
jbresolinn <juliabresolin2@gmail.com>
2020-08-11 16:45:50 -0300

    layout: fonts import config

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { AppLoading } from 'expo';
+
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { DMSans_400Regular, DMSans_700Bold } from '@expo-google-fonts/dm-sans';
+
 import Home from './src/pages/Home';
 
 export default function App() {
-  return (
-    <>
-      <Home />
-      <StatusBar style="light" />
-    </>
-  );
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    DMSans_400Regular,
+    DMSans_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <Home />
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }
